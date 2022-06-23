97c3fb47e1eda38a46354c91ded9184448eb3075
karthikpratihpati <karthikprathipati1@gmail.com>
2022-06-23 17:17:15 +0530

    added options still have to add css and a button

---
+++ src/components/option.js
@@ -0,0 +1,9 @@
+
+export default function Option(props){
+    return (
+        <div className="option">
+            <input type="button" value={props.value} />
+        </div>
+    );
+
+}
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,9 +1,18 @@
+import Option from "./option";
+
 export default function Quiz(props){
-    console.log(props.options);
+    const allAnswers =[...props.options,props.answer]
+    const allOptions = allAnswers.map((ele,index) =>{
+        return (
+            <Option value={ele} key={index} id ={index}  />
+        );
+    })
     return (
         <div className="quiz-card">
             <p className="quiz-que"> {props.question}</p>
-            <div className="quiz-options">{props.options} </div>
+            <div className="quiz-options">
+                {allOptions} 
+            </div>
         </div>
     );
 }
